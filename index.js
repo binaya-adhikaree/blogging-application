ad012f215589cf2386f0d54b35a3e8798f16a894
KaiZen <kaizen@KaiZen.KaiZen.js>
2025-01-03 21:33:06 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path")
+const app = express();
+const PORT = 8000;
+
+
+
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
+
+app.listen(PORT, (() => console.log(`server started at port : ${PORT}`)));
